312459fc1fb833936643e36faa25fddd9838f37c
eric180586 <sagereric02@gmail.com>
2025-12-01 18:29:45 -0800

    Updated TaskDetail.tsx

--- src/routes/TaskDetail.tsx
+++ src/routes/TaskDetail.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function TaskDetail() {
+  const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
-  // Hole Task-Details per ID (API/Hooks)
   return (
     <div>
-      <h2>Aufgabe: {id}</h2>
-      {/* Detailinfos, Aktionen usw. */}
-      <p>Hier erscheinen alle Infos zur gewählten Aufgabe.</p>
+      <h2>{t("task_title", { id })}</h2>
+      <p>{t("task_info")}</p>
     </div>
   );
 }
