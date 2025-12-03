ec764bd65f21a929896e39aa9fd02791f0c3c2ae
eric180586 <sagereric02@gmail.com>
2025-12-02 18:56:05 -0800

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+// src/main.tsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// Das ist der Einstiegspunkt für die gesamte App!
+// Stelle sicher, dass du im public/index.html ein <div id="root"></div> hast.
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
