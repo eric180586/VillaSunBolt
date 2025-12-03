abc8794b44d681d512d5bde368380cd46a92e25f
eric180586 <sagereric02@gmail.com>
2025-12-02 19:07:15 -0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,3 +9,24 @@ module.exports = {
   },
   plugins: [],
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beige: {
+          50: "#faf8f5",
+          100: "#f3eee6",
+          200: "#e6ded1",
+          800: "#6b5c4e",
+          900: "#554233"
+        }
+      }
+    }
+  },
+  plugins: [],
+}
