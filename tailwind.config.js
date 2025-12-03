3d1c6de325a9c21f2324f3405cd19e17a25f6ae4
eric180586 <sagereric02@gmail.com>
2025-12-02 19:21:33 -0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,25 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ganz wichtig, damit alle Komponenten abgedeckt sind!
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         beige: {
-          50: "#faf8f5",
-          100: "#f3eee6",
-          200: "#e6ded1",
-          800: "#6b5c4e",
-          900: "#554233"
-        }
-      }
-    }
+          50: '#FDF6EC',
+          100: '#F5E7D7',
+          200: '#E8D3B8',
+          300: '#DDC1A0',
+          400: '#CFA57E',
+          500: '#C0915F',
+          600: '#A47754',
+          700: '#856043',
+          800: '#6B4D36',
+          900: '#59402C',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
